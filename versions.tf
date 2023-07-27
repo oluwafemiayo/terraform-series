e78909552bd95126a6f9d41063a3b78f028905e1
admin <oluwafemifolorunso@oluwafems-iMac.local>
2023-07-27 10:58:39 +0300

    continuation

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
     required_version = "~> 1.4" #version locking
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
