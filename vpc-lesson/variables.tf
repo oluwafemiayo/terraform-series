d9dd5d2bf85228fb203ad0c0e87afe4d815bda3f
admin <oluwafemifolorunso@oluwafems-iMac.local>
2023-08-02 07:48:32 +0300

    RGF Technologies

---
+++ vpc-lesson/variables.tf
@@ -0,0 +1,7 @@
+#Input Variables
+#AWS Region
+variable "aws_region" {
+  description = "AWS Region where resource is being created"
+  type        = string
+  default     = "eu-west-3"
+}
---
+++ vpc-lesson/vpc.tf
@@ -0,0 +1,46 @@
+#Create AWS VPC Module
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
+
+  name   = "vpc-dev"
+  #region = "eu-west-1"
+
+  cidr                = "10.0.0.0/16"
+  azs                 = ["eu-west-3a", "eu-west-3b"]
+  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets      = ["10.0.101.0/24", "10.0.102.0/24"]
+  #Database Subnet
+  create_database_subnet_group = true
+  create_database_subnet_route_table= true 
+  database_subnets    = ["10.0.151.0/24", "10.0.152.0/24"]
+
+  # NAT Gateways - Outbound Communication
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+   # VPC DNS Parameters
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  public_subnet_tags = {
+    Type = "public-subnets"
+  }
+
+  private_subnet_tags = {
+    Type = "private-subnets"
+  }
+
+  database_subnet_tags = {
+    Type = "database-subnets"
+  }
+
+  tags = {
+    Owner = "sixwins"
+    Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "vpc-dev"
+  }
+}
